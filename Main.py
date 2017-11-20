db57fd706fc6de4613ac6617b12193bc6b8624d1
Théo VIEL <theo.viel@eleves.enpc.fr>
2017-11-20 18:16:05 +0100

    test4

---
+++ Main.py
@@ -0,0 +1,30 @@
+import os
+import sys
+
+os.chdir('C:\\Users\\teovi\\Documents\\IMI\\Projet MoPSi\\')
+
+from code.Fonctions import *
+from appli.Screen import *
+   
+
+
+
+B=bloc(2,1)
+
+
+
+
+
+
+QtGui.QApplication.setGraphicsSystem("raster")
+
+
+    
+app = QtGui.QApplication(sys.argv)
+
+Dialog = QtGui.QDialog()
+ui = Ui_Dialog(Dialog, app, B)
+Dialog.show()
+sys.exit(app.exec_())
+
+
