3885a8c044e69824949abf8890200f2334324fc3
Théo VIEL <theo.viel@eleves.enpc.fr>
2018-01-30 10:43:13 +0100

    programme d'optimisation des paramaetres de la loi

---
+++ testab.py
@@ -0,0 +1,37 @@
+import os
+import sys
+
+os.chdir('C:\\Users\\teovi\\Documents\\IMI\\Projet MoPSi\\')
+
+from code.Fonctions import *
+# from appli.Screen import *
+
+L=[]
+
+n=5
+m=5
+grid = grid(n,m)
+
+pieces=pieces_default(n,m)
+fixedpieces=[]
+
+bloc = bloc_default(n,m)
+        
+
+for i in range(20):
+    grid.clear()
+    compteur = 0
+    Pot=0
+    config_init(grid, pieces, fixedpieces, bloc)
+    Pot=grid.V()
+    while (Pot > 0):
+        compteur+=1
+        Pot = transform2(pieces, Pot, grid, bloc, compteur, fixedpieces)
+    print(compteur)
+    L.append(compteur)
+
+
+    
+    
+    
+
